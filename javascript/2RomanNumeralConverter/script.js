af7301b906a433b2f854d5b10c37af86a0bf7c2a
yanfang chen <juliechen209@gmail.com>
2024-10-16 21:19:02 -0500

    integer to Roman numeral converter

---
+++ javascript/2RomanNumeralConverter/script.js
@@ -0,0 +1,9 @@
+/**
+ * no value--"Please enter a valid number"
+-1:"Please enter a number greater than or equal to 1"
+>=4000: Please enter a number less than or equal to 3999
+16-XVI(x-10, v-5, i-1)
+649-DCXLIX
+1023-MXXIII
+3999-MMMCMXCIX
+ */
